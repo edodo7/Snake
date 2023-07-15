95c07833b84c0108366f5a6c27d7fc8d283d92b5
Eduardo <massa-messo@hotmail.com>
2023-07-16 01:28:38 +0200

    Key event detection is now working

--- src/main/java/com/example/snake/Controller.java
+++ src/main/java/com/example/snake/Controller.java
@@ -1,14 +1,33 @@
 package com.example.snake;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+
+    private @FXML Canvas canvas;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void changeColor(KeyEvent keyEvent) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        if (keyEvent.getCode() == KeyCode.LEFT){
+            gc.setFill(Color.BLACK);
+        }
+        if (keyEvent.getCode() == KeyCode.RIGHT){
+            gc.setFill(Color.YELLOW);
+        }
+        if (keyEvent.getCode() == KeyCode.UP){
+            gc.setFill(Color.RED);
+        }
+        if (keyEvent.getCode() == KeyCode.DOWN){
+            gc.setFill(Color.GREEN);
+        }
+        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
     }
+
+
 }
--- src/main/java/com/example/snake/Main.java
+++ src/main/java/com/example/snake/Main.java
@@ -8,13 +8,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        scene.getRoot().requestFocus();
     }
 
     public static void main(String[] args) {
