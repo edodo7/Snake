54d833cd50d9c149d06fd2b18315c2f380841c7b
Eduardo <massa-messo@hotmail.com>
2023-07-16 18:27:44 +0200

    Change JavaFX to libGDX

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.snake {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.snake to javafx.fxml;
-    exports com.example.snake;
-}
